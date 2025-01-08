4e50f8b87d10aada3752bd8c86d8363e437fb9aa
Aqib8860 <Aqibkhan8860@gmail.com>
2025-01-08 17:29:26 +0530

    update allowed host

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ALLOWED_HOST = ["localhost", "127.0.0.1"]
+ALLOWED_HOST = ["localhost", "127.0.0.1", "13.235.91.255", "ec2-13-235-91-255.ap-south-1.compute.amazonaws.com"]
 
 MONGODB_URL = os.environ.get("MONGODB_URL")
 DB_NAME = os.environ.get("DB_NAME")
